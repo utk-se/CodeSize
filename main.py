42988941eb855f0c774c7153272f2935e197f063
ashort96 <adamconnorshort@gmail.com>
2018-10-20 16:24:18 -0400

    csv works

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import lizard
-import lizard_ext
 import argparse
 import os
 
@@ -7,6 +6,7 @@ def parsearg():
     parser = argparse.ArgumentParser(description="CodeSize")
     parser.add_argument("-f")
     parser.add_argument("-d")
+    parser.add_argument("--csv", action='store_true')
     return parser.parse_args()
 
 def usageerror():
@@ -15,10 +15,24 @@ def usageerror():
     print("     -d {directory}")
     print("Exiting program...")
 
+def printfile(file):
+    lfile = lizard.analyze_file(file)
+    print("File: {}".format(file))
+    for func in lfile.function_list:
+        print("     Function Name: {}".format(func.long_name))
+        print("     Length: {}".format(func.length))
+
+def printfilecsv(file):
+    lfile = lizard.analyze_file(file)
+    for func in lfile.function_list:
+        print("{},\"{}\",{}".format(file, func.long_name, func.length))
+
+
 def main():
     args = parsearg()
     file = args.f
     directory = args.d
+    csv = args.csv
     # If neither are specified
     if file is None and directory is None:
         usageerror()
@@ -30,21 +44,23 @@ def main():
 
     # If it is a file
     if file is not None:
-        lfile = lizard.analyze_file(file)
-        for func in lfile.function_list:
-            print("File: {}".format(file))
-            print("     Function Name: {}".format(func.long_name))
-            print("     Length: {}".format(func.length))
+        if csv is True:
+            print("File, Function, Length")
+            printfilecsv(file)
+        else:
+            printfile(file)
+
 
     if directory is not None:
         for (root, subdir, files) in os.walk(directory):
+            if csv is True:
+                print("File, Function, Length")
             for file in files:
                 fullpath = os.path.join(root, file)
-                lfile = lizard.analyze_file(fullpath)
-                for func in lfile.function_list:
-                    print("File: {}".format(fullpath))
-                    print("     Function Name: {}".format(func.long_name))
-                    print("     Length: {}".format(func.length))
+                if csv is True:
+                    printfilecsv(fullpath)
+                else:
+                    printfile(fullpath)
 
 if __name__ == '__main__':
     main()
